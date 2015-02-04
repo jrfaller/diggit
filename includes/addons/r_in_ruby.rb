fc29a7965f8a47960d75b289930fac8e15e376c8
Matthieu Foucault <foucault.matthieu@gmail.com>
2015-02-04 17:04:52 +0100

    Fix SIGPIPE occurring with RinRuby Added Addon RWrapper (@addons[:R]) to work around this issue Fix magic string in diff_size.rb Added .rb to the list of extensions in diff_stats.rb

---
+++ includes/addons/r_in_ruby.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+R = nil # fixing SIGPIPE erroe in some cases. See http://hfeild-software.blogspot.fr/2013/01/rinruby-woes.html
+
+require "rinruby"
+
+class RWrapper < Diggit::Addon
+
+	def initialize(*args)
+		@r = RinRuby.new({:interactive=>false,:echo=>false})
+	end
+
+	def name
+		:R
+	end
+
+	def method_missing(meth, *args, &block)
+		@r.send meth, *args, &block
+	end
+
+end
