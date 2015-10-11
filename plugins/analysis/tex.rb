08da2a7832c8961f26fce4253debfce89f4970a9
Jean-Rémy Falleri <jr.falleri@gmail.com>
2015-10-11 17:12:28 +0200

    added analysis to count words in tex files.

---
+++ plugins/analysis/tex.rb
@@ -0,0 +1,48 @@
+# encoding: utf-8
+#
+# This file is part of Diggit.
+#
+# Diggit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Diggit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Diggit.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2015 Jean-Rémy Falleri <jr.falleri@gmail.com>
+
+require 'fileutils'
+
+class Tex < Diggit::Analysis
+	require_addons 'out'
+
+	def initialize(options)
+		super(options)
+	end
+
+	def run
+		walker = Rugged::Walker.new(repo)
+		walker.sorting(Rugged::SORT_TOPO | Rugged::SORT_REVERSE)
+		walker.push(repo.head.name)
+		walker.each do |c|
+			repo.checkout(c.oid, { strategy: [:force, :remove_untracked] })
+			words = Dir["**/*.tex"].reduce(0) { |a, e| a + `cat "#{e}" | wc -w`.to_i }
+			File.open(file, 'a') { |f| f.puts("#{source.url};#{c.oid};#{words}\n") }
+		end
+	end
+
+	def clean
+		FileUtils.rm_rf(file)
+		repo.checkout("master")
+	end
+
+	def file
+		"#{out.out}/words.csv"
+	end
+end
