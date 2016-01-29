cfeb3ff375513aa91162af8b03a011a3889059d9
Jean-Rémy Falleri <jr.falleri@gmail.com>
2016-01-29 10:44:54 +0100

    javadoc analysis update. now produce a json file.

--- plugins/analysis/javadoc.rb
+++ plugins/analysis/javadoc.rb
@@ -17,25 +17,79 @@
 #
 # Copyright 2015 Jean-Rémy Falleri <jr.falleri@gmail.com>
 
-require 'jsonpath'
+require 'nokogiri'
+require 'oj'
 
 class Javadoc < Diggit::Analysis
+	require_addons 'out'
+
+	VALID_TYPES = [
+			'root', 'CompilationUnit', 'TypeDeclaration', 'FieldDeclaration', 'MethodDeclaration',
+			'SimpleName', 'QualifiedName', 'SimpleType', 'PrimitiveType', 'SingleVariableDeclaration',
+			'VariableDeclarationFragment', 'Modifier', 'Javadoc', 'TagElement', 'TextElement', 'ArrayType'
+	]
+
 	def initialize(options)
 		super(options)
 	end
 
 	def run
-		path_md = JsonPath.new("$..*[?(@.typeLabel == 'MethodDeclaration')]")
-		files = Dir['**/*.java']
-		files.each do |file|
-			puts file
-			json = `gumtree parse "#{file}"`
-			md = path_md.on(json)
-			md.each do |m|
-				puts m
-				exit
+		files = Dir["#{@source.folder}/src/main/java/**/*.java"]
+		puts "#{files.length} files to process"
+		db = {}
+		files.each do |f|
+			puts "processing #{f}"
+			xml = `gumtree parse -f XML "#{f}"`
+			doc = Nokogiri::XML(xml)
+			db[f] = index_methods(strip(doc))
+		end
+		Oj.to_file("#{out.out}/#{@source.id}.json", db)
+	end
+
+	def index_methods(doc)
+		res = []
+		doc.xpath("//MethodDeclaration").each do |m|
+			id = method_id(m)
+			javadoc = {}
+			javadoc['main'] = m.xpath("Javadoc/TagElement[not(@label)]/TextElement/@label").to_s
+			javadoc['params'] = {}
+			m.xpath("Javadoc/TagElement[@label='@param']").each do |p|
+				javadoc['params'][p.at_xpath("SimpleName/@label").to_s] = p.xpath("TextElement/@label").to_s
+			end
+			javadoc['return'] = m.xpath("Javadoc/TagElement[@label='@return']/TextElement/@label").to_s
+			res << [id, javadoc]
+		end
+		res
+	end
+
+	def method_id(m)
+		res = ""
+		modifiers = m.xpath("Modifier/@label")
+		res += "#{modifiers} " unless modifiers.empty?
+		res += "#{type(m)} #{m.at_xpath('SimpleName/@label')}("
+		params = m.xpath("SingleVariableDeclaration").map { |p| "#{type(p)} #{p.at_xpath('SimpleName/@label')}" }.join(',')
+		res += "#{params})"
+		res
+	end
+
+	def type(e)
+		return e.at_xpath('SimpleType/@label').to_s unless e.at_xpath('SimpleType').nil?
+		return e.at_xpath('PrimitiveType/@label').to_s unless e.at_xpath('PrimitiveType').nil?
+		return e.at_xpath('ArrayType/@label').to_s unless e.at_xpath('ArrayType').nil?
+		""
+	end
+
+	def strip(doc)
+		active = []
+		doc.children.each do |c|
+			if VALID_TYPES.include?(c.name)
+				active << c
+			else
+				c.unlink
 			end
 		end
+		active.each { |a| strip(a) }
+		doc
 	end
 
 	def clean
