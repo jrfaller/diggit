90f74bc4de366260e01b09b2d67a628ce047d89d
Jean-Rémy Falleri <jr.falleri@gmail.com>
2018-01-19 15:49:45 +0100

    improve docker image

---
+++ docker/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dgit init
+# configure your sources here
+dgit sources add https://github.com/jrfaller/diggit.git
+# configure your analyses here
+dgit analyses add conflict_merge
+# launch dgit
+dgit clones perform
+dgit analyses perform
